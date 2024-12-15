ebbdec48487ec9d007065d59803a8168310ef426
Carlos Venegas <cvenegasa@udec.cl>
2024-12-15 16:39:55 -0300

    changed main subdataset for tuning pruposes

--- test_subdatasetMH.cpp
+++ test_subdatasetMH.cpp
@@ -12,39 +12,87 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    if (argc != 18) {
-        cerr << "Usage: " << argv[0] << " <dataset_file> <num_subdatasets> <threshold> <max_error> <temperature_pert> <temperature_leap> <cooling_rate> <heat_rate> <iteraciones_max> <time_max> <p> <pe> <pm> <rhoe> <X_INTVL> <X_NUMBER> <tuningMode>" << endl;
+
+    // Default values
+    int num_subdatasets = 1;
+    int threshold = 0.80;
+    int max_error = 30;
+    double temperature_pert = 400;
+    double temperature_leap = 800;
+    double cooling_rate = 0.95;
+    double heat_rate = 1.5;
+    int iteraciones_max = 300;
+    int time_max = 60;
+    unsigned p = 10;
+    double pe = 0.7;
+    double pm = 0.1;
+    double rhoe = 0.5;
+    unsigned X_INTVL = 10;
+    unsigned X_NUMBER = 5;
+    bool tuningMode = false;
+    string dataset_file;
+
+    // Verify that the minimum required arguments are provided
+    if (argc < 9) {
+        cerr << "Usage: " << argv[0] << "-alg <algorithm_name> -input <dataset_file> -th <threshold> -time <time_in_seconds> [optional parameters]" << endl;
+        cerr << "\nOptional parameters:" << endl;
+        cerr << "  -numsub <num>      Number of subdatasets" << endl;
+        cerr << "  -maxerr <num>      Maximum error" << endl;
+        cerr << "  -temppert <val>    Temperature perturbation" << endl;
+        cerr << "  -templeap <val>    Temperature leap" << endl;
+        cerr << "  -cool <val>        Cooling rate" << endl;
+        cerr << "  -heat <val>        Heat rate" << endl;
+        cerr << "  -maxiter <num>     Maximum iterations" << endl;
+        cerr << "  -p <num>           Parameter p" << endl;
+        cerr << "  -pe <val>          Parameter pe" << endl;
+        cerr << "  -pm <val>          Parameter pm" << endl;
+        cerr << "  -rhoe <val>        Parameter rhoe" << endl;
+        cerr << "  -xintvl <num>      X interval" << endl;
+        cerr << "  -xnum <num>        X number" << endl;
+        cerr << "  -tuning            Enable tuning mode" << endl;
         return 1;
     }
+    // Parse arguments
+    unordered_map<string, string> args;
+    for (int i = 1; i < argc; i += 2) {
+        if (i + 1 < argc) {
+            args[argv[i]] = argv[i + 1];
+        }
+    }
 
-    string dataset_file = argv[1];
-    int num_subdatasets = atoi(argv[2]);
-    int threshold = atoi(argv[3]);
-    int max_error = atoi(argv[4]);
-    double temperature_pert = atof(argv[5]);
-    double temperature_leap = atof(argv[6]);
-    double cooling_rate = atof(argv[7]);
-    double heat_rate = atof(argv[8]);
-    int iteraciones_max = atoi(argv[9]);
-    int time_max = atoi(argv[10]);
-    unsigned p = atoi(argv[11]);
-    double pe = atof(argv[12]);
-    double pm = atof(argv[13]);
-    double rhoe = atof(argv[14]);
-    unsigned X_INTVL = atoi(argv[15]);
-    unsigned X_NUMBER = atoi(argv[16]);
-    bool tuningMode = atoi(argv[17]);
-
+    // Mandatory parameters
+    string algorithm_name = args["-alg"];
+    dataset_file = args["-input"];
+    threshold = stod(args["-th"]);
+    time_max = stoi(args["-time"]);
     vector<string> dataset = loadInputData(dataset_file);
 
+    // Get the optional arguments
+    if (args.find("-numsub") != args.end()) num_subdatasets = stoi(args["-numsub"]);
+    if (args.find("-maxerr") != args.end()) max_error = stoi(args["-maxerr"]);
+    if (args.find("-temppert") != args.end()) temperature_pert = stod(args["-temppert"]);
+    if (args.find("-templeap") != args.end()) temperature_leap = stod(args["-templeap"]);
+    if (args.find("-cool") != args.end()) cooling_rate = stod(args["-cool"]);
+    if (args.find("-heat") != args.end()) heat_rate = stod(args["-heat"]);
+    if (args.find("-maxiter") != args.end()) iteraciones_max = stoi(args["-maxiter"]);
+    if (args.find("-p") != args.end()) p = stoi(args["-p"]);
+    if (args.find("-pe") != args.end()) pe = stod(args["-pe"]);
+    if (args.find("-pm") != args.end()) pm = stod(args["-pm"]);
+    if (args.find("-rhoe") != args.end()) rhoe = stod(args["-rhoe"]);
+    if (args.find("-xintvl") != args.end()) X_INTVL = stoi(args["-xintvl"]);
+    if (args.find("-xnum") != args.end()) X_NUMBER = stoi(args["-xnum"]);
+    if (args.find("-tuning") != args.end()) tuningMode = (args["-tuning"] == "true");
+
+    
     auto start = chrono::high_resolution_clock::now();
     solutionss result = recombinacion_subdataset(dataset, num_subdatasets, threshold, max_error, temperature_pert, temperature_leap, cooling_rate, heat_rate, iteraciones_max, time_max, p, pe, pm, rhoe, X_INTVL, X_NUMBER, tuningMode);
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<double> elapsed = end - start;
 
-    cout << "Best solution: " << result.solucion << endl;
-    cout << "Fitness: " << result.calidad << endl;
-    cout << "Elapsed time: " << elapsed.count() << " seconds" << endl;
-
+    if(!tuningMode){
+        cout << "Best solution: " << result.solucion << endl;
+        cout << "Fitness: " << result.calidad << endl;
+        cout << "Elapsed time: " << elapsed.count() << " seconds" << endl;
+    }
     return 0;
 }
