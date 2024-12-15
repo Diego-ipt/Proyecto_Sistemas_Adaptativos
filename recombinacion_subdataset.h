ce76ff9a538825f7656f61bbc27719b9a10b8ff3
Diego-ipt <106196745+Diego-ipt@users.noreply.github.com>
2024-12-15 05:40:28 -0300

    feat init_subdatasets

--- recombinacion_subdataset.h
+++ recombinacion_subdataset.h
@@ -16,6 +16,18 @@ using namespace std;
 
 //inicializador de subdataset junto con sus respectivas solucion/es inicial
 //usar divide_subdataset, luego por cada subdataset usar init_solucion_SuDa
+vector<Subdataset> init_subdatasets(const vector<string>& dataset, int num_subdatasets, int threshold,int max_error, 
+double temperature_pert,double temperature_leap , 
+double cooling_rate, double heat_rate, int iteraciones_max, 
+unordered_map<string, int> substring_to_index,
+unordered_map<int, string> index_to_substring){
+    vector<Subdataset> subdataset;
+    subdataset = divide_subdataset(dataset, num_subdatasets);
+    for(int i = 0; i < num_subdatasets; i++){
+        subdataset[i].soluciones.push_back(init_solucion_SuDa(subdataset[i].dataset, threshold, max_error, temperature_pert, temperature_leap, cooling_rate, heat_rate, iteraciones_max, substring_to_index, index_to_substring));
+    }
+    return subdataset;
+}
 
 //recombinacion de soluciones de subdatasets (ingreso de soluciones..
 //con la union de los subdatasets correspondientes..
